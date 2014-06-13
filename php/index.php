465a05f442e42a3b6d757760c99fd1bf5ba050fc
Chris Rasco <chris@chrisrasco.com>
2014-06-13 16:19:42 -0500

    Add default route with links to available checks

--- php/index.php
+++ php/index.php
@@ -27,4 +27,38 @@ $app->put('/service/checks/:id', function ($id) {
     echo "Updating check ".$id;
 });
 
+$app->delete('/service/checks/:id', function ($id) {
+    echo "Deleting check ".$id;
+});
+
+// Default route
+// TODO: Setup a template
+$app->get('/', function () {
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Expires" content="Fri, Jan 01 1900 00:00:00 GMT">
+<meta http-equiv="Pragma" content="no-cache">
+<meta http-equiv="Cache-Control" content="no-cache">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>PHP Sample App</title>
+</head>
+<body>
+<p>Available URIs</p>
+<ul>
+    <li><a href="service/checks">Get all checks (GET)</a></li>
+    <li><a href="service/checks/5">Get check (GET)</a></li>
+    <li><a href="service/checks/5">Update check (PUT)</a></li>
+    <li><a href="service/checks/5">Delete check (Delete)</a></li>
+    <li><a href="service/checks/5/status">Get status of check (GET)</a></li>
+    <li><a href="service/checks/5/execute">Execute Check (GET)</a></li>
+    <li><a href="service/checks/add">Add check</a></li>
+</ul>
+</body>
+</html>
+<?php
+});
+
+
 $app->run();
